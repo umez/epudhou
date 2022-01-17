a39dabd663ce151eb1091fa2b9c505b73cc7e0db
Neso <longjam2016@gmail.com>
2022-01-17 19:57:28 +0530

    language

--- src/app/shared/components/header/header.model.ts
+++ src/app/shared/components/header/header.model.ts
@@ -3,7 +3,7 @@ import { Nav } from "../nav/nav.model";
 
 export class HeaderModel {
   constructor(
-    public playMusic: boolean = true,
+    public playMusic: boolean = false,
     public lang: FormControl = new FormControl('en'),
     public showMenu: boolean = false
   ) {}
